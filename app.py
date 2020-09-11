cadd6fdcbf9298b81783d2ac2893a981b7b6f285
Mikhail Abramov <mabramov@redhat.com>
2020-09-11 13:15:20 +0200

    Heroku structure test

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("POSRGRES_URL")
+
+from .src.views import *
+
+if __name__ == '__main__':
+    app.run()
---
+++ manage.py
@@ -0,0 +1,15 @@
+from flask_script import Manager,commands
+from app import app
+
+manager = Manager(app)
+
+@manager.command
+def init_db():
+    with app.test_request_context():
+        from models import db
+        db.engine.echo = True
+        db.metadata.bind = db.engine
+        db.metadata.create_all(checkfirst=True)
+
+if __name__ == "__main__":
+    manager.run()
