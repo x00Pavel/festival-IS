e22148e301863785b2d28f4395e51cb56ea9ee5f
Mikhail Abramov <mabramov@redhat.com>
2020-09-11 12:17:29 +0200

    Clean Dev branch

--- festival-is-app/src/handlers.py
+++
@@ -1,5 +0,0 @@
-import flask
-import json
-
-def hello():
-    return flask.jsonify({"response": "Hello from Docker!"})
--- festival-is-db/2-data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO blog (id, title) VALUES (1, 'Misha');
-INSERT INTO blog (id, title) VALUES (2, 'Pasha');
-INSERT INTO blog (id, title) VALUES (3, 'Raul');
-INSERT INTO blog (id, title) VALUES (4, 'Raul');
-INSERT INTO blog (id, title) VALUES (5, 'RAUUUULLL');
