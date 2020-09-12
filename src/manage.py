715a34973656f889e2f64fa11a9da5334dba79e1
x00Pavel <xyadlo00@stud.fit.vutb.cz>
2020-09-12 14:27:06 +0200

    Update structure

--- src/manage.py
+++ src/manage.py
@@ -7,7 +7,7 @@ manager = Manager(app)
 @manager.command
 def init_db():
     with app.test_request_context():
-        from create_db import db
+        from src.festival_is_db.create_db import db
 
         db.engine.echo = True
         db.metadata.bind = db.engine
