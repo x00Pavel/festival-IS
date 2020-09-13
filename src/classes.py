0ccca42cb41d0d0c98395d2aba5fd2556976db56
x00Pavel <xyadlo00@stud.fit.vutb.cz>
2020-09-13 16:56:17 +0200

    Renamed to classes

--- src/classes.py
+++ src/classes.py
@@ -1,8 +1,26 @@
+class Ticket:
+    pass
+
+
 class User:
+    """
+    Basic unauthorized user 
+    """
+
     perms = 5
+
+    def buy_ticket(self):
+        pass
+
+    def register_fest(self):
+        pass
 
 
 class AuthUser(User):
+    """
+    Basic authorized user 
+    """
+
     perms = 4
 
 
@@ -20,5 +38,3 @@ class Admin(Organizer):
 
 class RootAdmin(Organizer):
     perms = 0
-
-    def __init__(self):
