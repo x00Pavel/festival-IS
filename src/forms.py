a0fc0f4fa41fd2139e0d2076856c052713a99c04
fure0061 <rulik-a@mail.ru>
2020-09-24 13:28:29 +0200

    Added new code, bugs fixed

--- src/forms.py
+++ src/forms.py
@@ -5,38 +5,50 @@ from wtforms.fields import *
 from wtforms import ValidationError
 import phonenumbers
 from flask import request
+
+
 class RegistrationForm(FlaskForm):
-	username = StringField('Username', 
-							validators=[DataRequired(), Length(min=2,max=20)])
-	email = StringField('Email', validators=[DataRequired(), Email()])
-	password = PasswordField('Password', validators=[DataRequired(), Length(min=2,max=20)])
-	passwordC = PasswordField('Password Confirmation',
-				validators=[DataRequired(), Length(min=2,max=20), EqualTo('password')])
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField(
+        "Password", validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    passwordC = PasswordField(
+        "Password Confirmation",
+        validators=[DataRequired(), Length(min=2, max=20), EqualTo("password")],
+    )
 
-	submit = SubmitField('Sign up')
-	firstname = StringField('First name', validators=[DataRequired(), Length(max=50)])
-	lastname= StringField('Last name', validators=[DataRequired(), Length(max=50)])
-	city= StringField('City', validators=[Length(max=80)])
-	street= StringField('Street', validators=[Length(max=80)])
-	streeta= StringField('Street (additional)', validators=[Length(max=80)])
-	homenum = StringField('Home/Flat number', validators=[Length(max=80)])
+    submit = SubmitField("Sign up")
+    firstname = StringField("First name", validators=[DataRequired(), Length(max=50)])
+    lastname = StringField("Last name", validators=[DataRequired(), Length(max=50)])
+    city = StringField("City", validators=[Length(max=80)])
+    street = StringField("Street", validators=[Length(max=80)])
+    streeta = StringField("Street (additional)", validators=[Length(max=80)])
+    homenum = StringField("Home/Flat number", validators=[Length(max=80)])
 
+    # phone = StringField("Phone", validators=[DataRequired()])
 
-	phone = StringField('Phone', validators=[DataRequired()])
-	phonenumber = IntegerField('Phone number')
-	def validate_phonenumber(self,phonenumbers):
-		try:
-			p = phonenumbers.parse(phone.data)
-			if not phonenumbers.is_valid_number(p):
-				raise ValueError()
-		except(phonenumbers.phonenumberutil.NumberParseException, ValueError):
-			raise ValidationError('Invalid phone number')
+    phonenumber = StringField("Phone number")
 
+    def validate_phone(self, form, field):
+        if len(field.data) > 16:
+            raise ValidationError("Invalid phone number.")
+        try:
+            input_number = phonenumbers.parse(field.data)
+            if not (phonenumbers.is_valid_number(input_number)):
+                raise ValidationError("Invalid phone number.")
+        except:
+            input_number = phonenumbers.parse("+1" + field.data)
+            if not (phonenumbers.is_valid_number(input_number)):
+                raise ValidationError("Invalid phone number.")
 
-		
 
 class LoginForm(FlaskForm):
-	email = StringField('Email', validators=[DataRequired(), Email()])
-	password = PasswordField('Password', validators=[DataRequired(), Length(min=2,max=20)])
-	remember = BooleanField('Remember me')
-	submit = SubmitField('Log in up')
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField(
+        "Password", validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    remember = BooleanField("Remember me")
+    submit = SubmitField("Log in up")
--- src/views.py
+++ src/views.py
@@ -34,6 +34,7 @@ def register():
     if form.validate_on_submit():
         flash(f"Account created for {form.username.data}!", "success")
         return redirect(url_for("home"))
+    flash(f"Account craaeated for {form.errors}!", "success")
     return render_template("register.html", title="Registration", form=form)
 
 
